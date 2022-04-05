2b3235f31be3eacece97ca78ef16bb6c38364f94
Sheharyar Khan <sheharyar.alam.khan@gmail.com>
2022-04-05 04:55:58 -0700

    fixed repeated letter glitch

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -3,16 +3,17 @@ import { AppContext } from "../App"
 
 
 function Letter({letterPos, attemptVal }) {
-	const { board, setBoard, wordOfTheDay, currAttempt, disabledLetters, setDisabledLetters } = useContext(AppContext)
+	const { board, wordOfTheDay, currAttempt, setDisabledLetters } = useContext(AppContext)
 	const letter = board[attemptVal][letterPos]
 
 	const correct = wordOfTheDay[letterPos] === letter
 	const almost = !correct && letter !== "" && wordOfTheDay.includes(letter)
-	let extra = wordOfTheDay.filter(x => x === letter).length < board[attemptVal].filter(x => x === letter).length
+	// let extra = wordOfTheDay.filter(x => x === letter).length < board[attemptVal].filter(x => x === letter).length
+	const not_extra = (board[attemptVal].slice(0, letterPos).filter(x => x === letter).length < wordOfTheDay.filter(x => x === letter).length)
 	let c = 'letter '
 	const letterState = 
 		currAttempt.attempt > attemptVal &&
-		(correct ? "correct" : (almost && !extra)? "almost" : "error" )
+		(correct ? "correct" : (almost && not_extra)? "almost" : "error" )
 
 	useEffect(() => {
 		if (letter !== "" && !correct && !almost) {
